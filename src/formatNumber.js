acf66ae61082366d7b42f761963fb04fac8824e6
EIRojo <laveaga.mendoza.carlos@gmail.com>
2024-02-26 13:46:00 -0700

    format object for import

--- src/formatNumber.js
+++ src/formatNumber.js
@@ -1,4 +1,9 @@
 export const formatNumber= (importe) =>{
+
+	if (typeof importe === 'object' && importe.$$typeof === Symbol.for('react.element')) {
+        // If importe is a React element, extract the value from props.children
+        importe = importe.props.children;
+    }
     
 		   
 		return ((Number(importe)).toLocaleString('en-US', {
